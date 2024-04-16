fab0ec9dc6925f18bd768b6344d600d7a0ca7af6
Divyagadiyamula <divyaagadiyamula@gmail.com>
2024-04-16 16:03:30 +0530

    js

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-var a=10;
-console.clear(a);
+//var a=10;
+//var b=30;
+//console.warn(a,b);
+//console.error(b)
+//let a;
+//a=10;
+//console.log(a)
+//let a="divya";
+//arrays
+// let a;
+// a=30;
+// console.log(typeof a)
+// let b;
+// b="30";
+// console.log(typeof b)
+// let arr1=[1,2,3,"divya"]
+// console.log(arr1)
+// console.log(typeof arr1)
+// let y;
+// y=null;
+// console.log(typeof y)
+// //objects
+// let z={
+//     name:"divya",
+//     num:8639955347,
+// }
+// console.log(z)
+// console.log(z,typeof z)
+// console.log(typeof(typeof z))
+var x=prompt()
+console.log(x,typeof x);
