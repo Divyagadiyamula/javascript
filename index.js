66933202d8de8f8784bdc2e9ad377b4964f8a723
Divyagadiyamula <divyaagadiyamula@gmail.com>
2024-04-17 10:18:01 +0530

    js

--- index.js
+++ index.js
@@ -2,23 +2,16 @@
 //var b=30;
 //console.warn(a,b);
 //console.error(b)
-//let a;
+
 //a=10;
-//console.log(a)
+
 //let a="divya";
 //arrays
-// let a;
-// a=30;
-// console.log(typeof a)
-// let b;
-// b="30";
-// console.log(typeof b)
 // let arr1=[1,2,3,"divya"]
 // console.log(arr1)
 // console.log(typeof arr1)
-// let y;
-// y=null;
-// console.log(typeof y)
+ let y=true;
+ console.log(typeof y)
 // //objects
 // let z={
 //     name:"divya",
@@ -27,5 +20,5 @@
 // console.log(z)
 // console.log(z,typeof z)
 // console.log(typeof(typeof z))
-var x=prompt()
-console.log(x,typeof x);
+// var x=prompt()
+// console.log(x,typeof x);
