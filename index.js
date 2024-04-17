e3ed956083797c8f3a337ed2c047b5fc48fccb67
Divyagadiyamula <divyaagadiyamula@gmail.com>
2024-04-17 21:00:34 +0530

    js

--- index.js
+++ index.js
@@ -7,18 +7,90 @@
 
 //let a="divya";
 //arrays
-// let arr1=[1,2,3,"divya"]
-// console.log(arr1)
-// console.log(typeof arr1)
- let y=true;
- console.log(typeof y)
+ //let arr1=[1,2,3,"divya"]
+ //console.log(arr1)
+ //console.log(typeof arr1)
+// let y=true;
+ //console.log(typeof y)
 // //objects
-// let z={
-//     name:"divya",
-//     num:8639955347,
-// }
-// console.log(z)
-// console.log(z,typeof z)
+//let z={
+    //name:"divya",
+    //num:8639955347,
+//}
+ //console.log(z)
+ //console.log(z,typeof z)
 // console.log(typeof(typeof z))
 // var x=prompt()
 // console.log(x,typeof x);
+//let x = 123422222222222222222222222222222222222n
+//console.log(typeof x)
+//debugging 17/04/24
+//debugger;
+//var a=10; 
+//console.log(a);
+//operators
+//addition
+let a=3,b=4;
+let sum=a+b;
+console.log(sum);
+//console.log("sum of the two numbers is",sum);
+//console.log('sum of ${a} and ${b} is ${sum}');
+//substaction
+//let sub=a-b;
+//console.log('sub of ${a} and ${b} is ${sub}');
+//multiplication
+//let mul=a*b;
+//console.log('prod of ${a} and ${b} is ${mul}');
+//division
+//let div=a%b;
+//console.log('div of ${a} and ${b} is ${div}');
+//reminder
+//let rem=10%2;
+//console.log('rem of ${a} and ${b} is ${rem}');
+//decrement
+//let x=5;
+//x--;
+//console.log(x);
+//pre-increment
+//let q=5;
+//let y=++q;
+//console.log(q)
+//post-increment
+//let c=5;
+//let d=c++;
+//console.log(d,c);
+//assignment operators
+//let z=5;
+//z**=2
+//console.log(z**2) ;
+//we can compare the values in two ways
+//double equals to == :it checks the valueand data type
+//triple equals to === it checks only value 
+//let a=2;
+//let h="divya";
+//console.log(a==b);
+//console.log(a===b);
+//console.log(a!==b);
+//console.log(a<2 && b>2);
+//console.log(!(a<2 && b>2));
+//concatination two add two strings
+//let g="hello";
+//let i="world";
+//sum= g+i;
+//console.log(sum); 
+//terinary operator :it will execute the statements based on the condition
+//(3>4)?console.log("false statement"):console.log("true statement");
+//let allow=true;
+//allow?console.log("phone is tracking by operator"):console.log("phone is unable to tracking");
+//conditional statements
+
+
+
+
+
+
+
+
+
+
+
